5531a33e378f6829b8836ebd4bc0afb365da4d44
Patrick Beeson <patrickbeeson@gmail.com>
2014-11-02 10:51:43 -0500

    Add races app and model.

---
+++ racemanager/races/models.py
@@ -0,0 +1,150 @@
+import datetime
+from geopy.geocoders import Nominatim
+from localflavor.us.models import USStateField, USPhoneNumberField
+
+from django.db import models
+
+
+class Season(models.Model):
+    """
+    A season to hold races for a given year range.
+    """
+    opening_year = models.PositiveSmallIntegerField(
+        help_text='Can be used for primary year if needed. Ex. 2014.',
+    )
+    closing_year = models.PositiveSmallIntegerField(
+        help_text='Optional. Ex. 2015.',
+        blank=True,
+        null=True
+    )
+    slug = models.SlugField(
+        help_text='Will populate from opening and/or closing year fields.',
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['opening_year']
+
+    def __str__(self):
+        return ('{}-{}'.format(self.opening_year, str(self.closing_year)[2:4]))
+
+
+class Organizer(models.Model):
+    """
+    A person or entity organizing the race.
+    """
+    name = models.CharField(
+        max_length=200,
+        help_text='Limited to 200 characters. Should be a person, ideally.',
+        default=''
+    )
+    phone = USPhoneNumberField(
+        help_text='Optional. Phone numbers must be in XXX-XXX-XXXX format.',
+        blank=True,
+        null=True
+    )
+    email = models.EmailField(
+        help_text='Optional.'
+        blank=True,
+        default=''
+    )
+    website = models.URLField(
+        help_text='Optional.',
+        blank=True,
+        default=''
+    )
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    """
+    A race location.
+    """
+    city = models.CharField(
+        max_length=100,
+        help_text='Limited to 100 characters.',
+        default=''
+    )
+    state = USStateField(
+        default='NC'
+    )
+    zip_code = models.PositiveIntegerField(
+        max_length=5,
+        help_text='Limited to 5 characters.'
+    )
+    address = models.CharField(
+        max_length=200,
+        help_text='Limited to 200 characters. The numbered street \
+        address of the race. For example: 151 Piedmont Ave.',
+        default=''
+    )
+    description = models.TextField(
+        help_text='Optional. A brief description of the location. This is \
+        to help racers find the course.',
+        blank=True,
+        default=''
+    )
+    latitude = models.FloatField(
+        blank=True,
+        null=True
+    )
+    longitude = models.FloatField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ['city']
+
+    def __str__(self):
+        return self.address
+
+    def save(self, *args, **kwargs):
+        """
+        Geocode the location based on address and zip code.
+        """
+        geolocator = Nominatim()
+        location = geolocator.geocode('{} {}'.format(self.address, self.zip_code))
+        self.latitude = location.latitude
+        self.longitude = location.longitude
+        super(Location, self).save(*args, **kwargs)
+
+
+class Race(models.Model):
+    """
+    A race within a series.
+    """
+    date = models.DateField()
+    season = models.ForeignKey(Season)
+    location = ForeignKey(Location)
+    organizer = ForeignKey(Organizer)
+    description = models.TextField(
+        default='A brief description of the race course, ideally.'
+    )
+    pre_registration_link = models.URLField(
+        help_text='Optional.'
+        blank=True,
+        default=''
+    )
+    flyer_upload = models.FileField(
+        help_text='Optional. PDF files only.',
+        blank=True,
+        default=''
+    )
+    results_link = models.URLField(
+        help_text='Optional.',
+        blank=True,
+        default=''
+    )
+    results_upload = models.FileField(
+        help_text='Optional. PDF files only.',
+        blank=True,
+        default=''
+    )
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return ('{}, {}'.format(self.date.strftime('%Y %b %d'), self.location.city))
