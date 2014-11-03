2f499fd6a01339da6fb09fe2bc81c29d75582c4f
Patrick Beeson <patrickbeeson@gmail.com>
2014-11-03 07:01:40 -0500

    Create detail view and admin for races app.

--- racemanager/races/admin.py
+++ racemanager/races/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from races.models import Race, Location, Organizer, Season
+
+
+class SeasonAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrganizerAdmin(admin.ModelAdmin):
+    pass
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('city', 'address')
+
+
+class RaceAdmin(admin.ModelAdmin):
+    list_filter = ('date','season','location')
+    search_fields = ('location', 'description')
+    list_display = ('date', 'location', 'season')
--- racemanager/races/views.py
+++ racemanager/races/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from races.models import Race
+
+
+class RaceDetail(DetailView):
+    model = Race
