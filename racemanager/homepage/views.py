99f5515c672daae86f0f6ab95f06726df17813e1
Patrick Beeson <patrickbeeson@gmail.com>
2014-12-27 06:41:57 -0500

    More changes to homepage views.

--- racemanager/homepage/views.py
+++ racemanager/homepage/views.py
@@ -8,7 +8,6 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['race_list'] = Race.upcoming_races.all()
         context['weekend_race_list'] = Race.upcoming_races_for_weekend.all()
         context['remaining_races_for_month_list'] = Race.upcoming_races_for_month.all()
         # Need to add context for results, model too?
