b6bd8db208777f84e2d0cd721b9f8d7296e6e710
Patrick Beeson <patrickbeeson@gmail.com>
2014-12-28 12:32:28 -0500

    Add tests for homepage views. Update test settings to avoid warning.

--- racemanager/homepage/tests.py
+++ racemanager/homepage/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test import RequestFactory
 
-# Create your tests here.
+from homepage.views import HomePageView
+
+
+class HomePageText(TestCase):
+
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.view = HomePageView.as_view()
+
+    def test_home_page_renders_template(self):
+        response = self.view(self.request)
+        self.assertEqual(response.template_name, ['home.html'])
+
+    def test_home_page_returns(self):
+        response = self.view(self.request)
+        self.assertEqual(response.status_code, 200)
--- racemanager/racemanager/settings/test.py
+++ racemanager/racemanager/settings/test.py
@@ -17,3 +17,5 @@ DATABASES = {
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
