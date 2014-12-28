4579cc7421263364695bbe8db932fae23540122a
Patrick Beeson <patrickbeeson@gmail.com>
2014-12-28 08:50:07 -0500

    Add basic url patterns to global patterns, including homepage views. Update race views for results and race list.

--- racemanager/racemanager/urls.py
+++ racemanager/racemanager/urls.py
@@ -1,10 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from homepage.views import HomePageView
+
+admin.site.header = 'NCCX administration'
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'racemanager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^administration/', include(admin.site.urls)),
+    url(r'^administration/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
--- racemanager/races/views.py
+++ racemanager/races/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from races.models import Race
+from races.models import Race, Season
 
 
 class RaceDetail(DetailView):
     model = Race
+
+
+class CurrentSeasonRaceList(DetailView):
+    model = Season
+    queryset = Season.current_season.all()
+    template_name = 'upcoming_race_list.html'
+
+
+class ResultsList(ListView):
+    model = Season
